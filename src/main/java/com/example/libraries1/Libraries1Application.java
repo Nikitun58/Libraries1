6b7152e7b37a87e5e0467e8a7946ca51feaa5b8e
nikit <Nikitaaa6@yandex.ru>
2023-08-11 00:07:20 +0800

    1

---
+++ src/main/java/com/example/libraries1/Libraries1Application.java
@@ -0,0 +1,13 @@
+package com.example.libraries1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Libraries1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Libraries1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/libraries1/Libraries1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraries1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Libraries1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
